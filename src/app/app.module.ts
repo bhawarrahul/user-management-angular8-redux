4b97605509086e17a6a9dbd73ef5cc82372c7562
rahul.bhawar <rahul.bhawar@krawlernetworks.com>
2020-01-13 15:10:28 +0530

    Module import changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AddUserComponent } from './components/add-user/add-user.component';
-import { UserListComponent } from './components/user-list/user-list.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AddUserComponent } from "./components/add-user/add-user.component";
+import { UserListComponent } from "./components/user-list/user-list.component";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { FlexLayoutModule } from "@angular/flex-layout";
 import {
-  MatAutocompleteModule,
   MatButtonModule,
-  MatButtonToggleModule,
   MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
   MatDialogModule,
-  MatExpansionModule,
-  MatGridListModule,
   MatIconModule,
   MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
   MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
   MatSortModule,
   MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatStepperModule,
   MatFormFieldModule
-  } from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { addUserReducer } from './state/reducers/user-reducers';
-import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
-
-
+} from "@angular/material";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { addUserReducer } from "./state/reducers/user-reducers";
+import { ConfirmDialogComponent } from "./components/confirm-dialog/confirm-dialog.component";
 
 @NgModule({
   declarations: [
@@ -60,37 +35,14 @@ import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dial
     ConfirmDialogComponent
   ],
   imports: [
-    MatAutocompleteModule,
     MatButtonModule,
-    MatButtonToggleModule,
     MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
-    MatDatepickerModule,
     MatDialogModule,
-    MatExpansionModule,
-    MatGridListModule,
     MatIconModule,
     MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
     MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
     MatSortModule,
     MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
     BrowserModule,
     MatFormFieldModule,
     FormsModule,
@@ -99,21 +51,21 @@ import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dial
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularFontAwesomeModule,
-    StoreModule.forRoot({user: addUserReducer}),
+    StoreModule.forRoot({ user: addUserReducer }),
     HttpClientModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        })
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [AddUserComponent,ConfirmDialogComponent]
+  entryComponents: [AddUserComponent, ConfirmDialogComponent]
 })
-export class AppModule { }
+export class AppModule {}
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
